8076a1a452f74d262fffa0a70390c1d15ee1f255
Jazmin Celaya Sanchez <jazmin.celaya@MacBook-Pro-de-Jazmin.local>
2021-10-27 21:08:58 -0500

    destructures props, wraps up unit tests

--- src/components/Cards/Cards.component.jsx
+++ src/components/Cards/Cards.component.jsx
@@ -7,21 +7,24 @@ import {
 import Card from './Card.component';
 import mock from './mock.data';
 
-function Cards() {
-  const { items } = mock;
+function Cards({ items }) {
+  const itemsArray = items || mock.items;
   return (
     <StyledCardsGeneralContainer data-testid="cards">
-      <StyledWelcomeHeader data-testid="welcome-header">Welcome to the Challenge!</StyledWelcomeHeader>
+      <StyledWelcomeHeader data-testid="welcome-header">
+        Welcome to the Challenge!
+      </StyledWelcomeHeader>
       <StyledCardsContainer>
-        {items.map((item) => {
+        {itemsArray.map((item, i) => {
           const { etag, snippet } = item;
           const { thumbnails, title, description } = snippet;
+          const key = etag + i;
           return (
             <Card
               image={thumbnails.high.url}
               description={description}
               title={title}
-              key={etag}
+              key={key}
             />
           );
         })}
--- src/components/Cards/__test__/Cards.test.jsx
+++ src/components/Cards/__test__/Cards.test.jsx
@@ -12,8 +12,69 @@ test('renders Cards component without crashing', () => {
   expect(cardsElement).toBeInTheDocument();
 });
 
-test('renders Welcome tittle', () => {
+test('renders Welcome title', () => {
   render(<Cards />);
   const cardsElement = screen.getByTestId('welcome-header');
   expect(cardsElement).toHaveTextContent('Welcome to the Challenge!');
+});
+
+test('renders mock data when no props are passed', () => {
+  render(<Cards />);
+  const cardsElements = screen.getAllByTestId('card');
+  expect(cardsElements.length).toBe(25);
+});
+
+test('renders mock data when props are undefined', () => {
+  render(<Cards items={undefined} />);
+  const cardsElements = screen.getAllByTestId('card');
+  expect(cardsElements.length).toBe(25);
+});
+
+test('renders mock data when props are null', () => {
+  render(<Cards items={null} />);
+  const cardsElements = screen.getAllByTestId('card');
+  expect(cardsElements.length).toBe(25);
+});
+
+test('renders no cards when props array is empty', () => {
+  render(<Cards items={[]} />);
+  const cardsElements = screen.queryByTestId('card');
+  expect(cardsElements).not.toBeInTheDocument();
+});
+
+test('renders card with passed props', () => {
+  const testData = [
+    {
+      kind: 'youtube#searchResult',
+      etag: '_PVKwNJf_qw9nukFeRFOtQ837o0',
+      id: {
+        kind: 'youtube#channel',
+        channelId: 'UCPGzT4wecuWM0BH9mPiulXg',
+      },
+      snippet: {
+        publishedAt: '2014-09-27T01:39:18Z',
+        channelId: 'UCPGzT4wecuWM0BH9mPiulXg',
+        title: 'Wizeline',
+        description:
+          "Wizeline transforms how teams build technology. Its customers accelerate the delivery of innovative products with proven solutions, which combine Wizeline's ...",
+        thumbnails: {
+          default: {
+            url: 'https://yt3.ggpht.com/ytc/AAUvwnighSReQlmHl_S_vSfvnWBAG5Cw4A0YxtE0tm5OpQ=s88-c-k-c0xffffffff-no-rj-mo',
+          },
+          medium: {
+            url: 'https://yt3.ggpht.com/ytc/AAUvwnighSReQlmHl_S_vSfvnWBAG5Cw4A0YxtE0tm5OpQ=s240-c-k-c0xffffffff-no-rj-mo',
+          },
+          high: {
+            url: 'https://yt3.ggpht.com/ytc/AAUvwnighSReQlmHl_S_vSfvnWBAG5Cw4A0YxtE0tm5OpQ=s800-c-k-c0xffffffff-no-rj-mo',
+          },
+        },
+        channelTitle: 'Wizeline',
+        liveBroadcastContent: 'upcoming',
+        publishTime: '2014-09-27T01:39:18Z',
+      },
+    },
+  ];
+  render(<Cards items={testData} />);
+  const cardsElements = screen.getAllByTestId('card');
+  expect(cardsElements.length).toBe(1);
 });
