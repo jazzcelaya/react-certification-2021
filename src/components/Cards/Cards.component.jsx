ad9de168ae06a7318aa5167d517d3ee931ef446b
Jazmin Celaya Sanchez <jazmin.celaya@MacBook-Pro-de-Jazmin.local>
2021-10-29 02:21:37 -0500

    FEAT fetches video list from api

--- src/components/Cards/Cards.component.jsx
+++ src/components/Cards/Cards.component.jsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {
   StyledCardsContainer,
   StyledCardsGeneralContainer,
   StyledWelcomeHeader,
 } from '../../styled/Cards.styled';
 import Card from './Card.component';
-import mock from './mock.data';
+import { mockData } from './mock.data';
 
-function Cards({ items }) {
-  const itemsArray = items || mock.items;
+const baseUrl = `https://www.googleapis.com/youtube/v3/search?maxResults=25&key=${process.env.REACT_APP_API_KEY}&part=snippet`;
+
+async function getVideos() {
+  try {
+    const response = await axios({
+      url: baseUrl,
+      method: 'GET',
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+function Cards() {
+  const [items, setItems] = useState(mockData.items);
+
+  useEffect(() => {
+    async function loadVideos() {
+      const response = await getVideos();
+      if (response.status === 200) {
+        setItems(response.data.items);
+      }
+    }
+    loadVideos();
+  },[]);
+
   return (
     <StyledCardsGeneralContainer data-testid="cards">
       <StyledWelcomeHeader data-testid="welcome-header">
         Welcome to the Challenge!
       </StyledWelcomeHeader>
       <StyledCardsContainer>
-        {itemsArray.map((item, i) => {
+        {items.map((item, i) => {
           const { etag, snippet } = item;
           const { thumbnails, title, description } = snippet;
           const key = etag + i;
